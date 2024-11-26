1dcf29f724bd808555a8ee22ea635909f1ef827b
Przemyslaw Klosowski <Przemyslaw.Klosowski@unilever.com>
2024-11-26 11:03:21 +0100

    auto commit

--- app.js
+++ app.js
@@ -219,7 +219,7 @@ app.post('/evacuation/start', (req, res) => {
     devices.forEach(device => {  
       previousUrls[device.id] = device.currentUrl; // Zapisz poprzedni URL do obiektu  
       axios.post(`http://${device.ip}:3000/change-url`, {  
-        url: 'http://156.4.10.182:8084/dash/ewakuacja_test.html'  
+        url: 'http://156.4.10.182:8084/dash/ewakuacja.html'  
       }).catch(err => {  
         console.error(`Błąd zmiany URL dla urządzenia ${device.id}:`, err.message);  
       });  
@@ -229,7 +229,7 @@ app.post('/evacuation/start', (req, res) => {
     // Zmień URL na stronę ewakuacyjną bez zapisywania poprzednich URL-i  
     devices.forEach(device => {  
       axios.post(`http://${device.ip}:3000/change-url`, {  
-        url: 'http://156.4.10.182:8084/dash/ewakuacja_test.html'  
+        url: 'http://156.4.10.182:8084/dash/ewakuacja.html'  
       }).catch(err => {  
         console.error(`Błąd zmiany URL dla urządzenia ${device.id}:`, err.message);  
       });  
